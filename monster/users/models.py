a54ce14ca116bd4ed5eb1d329613b1b6228c6e22
Ale_Ksandr <dark7000000001@gmail.com>
2024-02-03 14:43:13 +0700

    Сделал регистрация по email

---
+++ monster/users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from users.managers import UserManager
+    
+    
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), blank=False, null=False, unique=True)
+    username = None
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    
+    objects = UserManager()
+    
+    class Meta:
+        verbose_name_plural = 'Пользователь'
+        verbose_name = 'Пользователь'
+
+    def __str__(self):
+        return f'{self.email}'
